5249d86247c65531ac2ff6d740e49b7c50ba3b20
Camila <camila.warmeling04@gmail.com>
2026-06-27 15:40:43 -0300

    implementação dos métodos de verificar linha vazia na coluna e de posicionar peça

--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -1,5 +1,6 @@
 public class Tabuleiro {
     private char[][] tabuleiro = new char[6][7]; //[linha][coluna]
+    private int linhaVazia, colunaVazia;
     
     public Tabuleiro(){
         criarTabuleiro();
@@ -33,14 +34,22 @@ public class Tabuleiro {
         return colunaExiste;
     }
         
-    public boolean verificarEspacoDisponivelNaColuna(int coluna){
-        boolean colunaDisponivel;
-        //TO DO
+    public boolean verificarEspacoDisponivelNaColuna(int coluna){        
+        boolean colunaDisponivel = false;
+        
+        for(int linha=5; linha>=0; linha--){
+            colunaDisponivel = tabuleiro[linha][coluna] == 'B';
+            if(colunaDisponivel){//quando encontra a coluna disponível acaba o for
+                linhaVazia = linha;
+                colunaVazia = coluna;
+                break;
+            }
+        }
         return colunaDisponivel;
     }
 
-    public void posicionarPecaNoTabuleiro(char corPeca){
-
+    public void posicionarPecaNoTabuleiro(char corPosicionar){
+        tabuleiro[linhaVazia][colunaVazia] = corPosicionar;
     }
 
 
